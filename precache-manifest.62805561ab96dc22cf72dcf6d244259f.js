05de6744540b4b609aa4c0a431490cc5d5e6ee3c
Austin <austin.hokun.wong@gmail.com>
2020-09-02 16:27:40 -0400

    Updates

---
+++ precache-manifest.62805561ab96dc22cf72dcf6d244259f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48d7a08d72e913e27a70664d654e84ac",
+    "url": "/kanban/index.html"
+  },
+  {
+    "revision": "5b58d4fcfe520d3ee8a4",
+    "url": "/kanban/static/css/main.0891ac1d.chunk.css"
+  },
+  {
+    "revision": "f99a63178fe0acca6bbd",
+    "url": "/kanban/static/js/2.7035feeb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kanban/static/js/2.7035feeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b58d4fcfe520d3ee8a4",
+    "url": "/kanban/static/js/main.58037e54.chunk.js"
+  },
+  {
+    "revision": "2f0fcd786d27d41c3e0a",
+    "url": "/kanban/static/js/runtime-main.0acb4caf.js"
+  }
+]);
